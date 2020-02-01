7457f4ee3bf5b84b49bae08306486ed10a6ec662
hugr2301 <romain.hugi@usherbrooke.ca>
2020-02-01 15:37:16 -0500

    Initial commit

---
+++ RepairTheFuckingWorld/Repair The Fucking World/Assets/Scripts/EventsList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsList
+{
+    private static EventsList instance = null;
+    private static readonly Object mutex = new Object();
+    private Dictionary<int, Event> Events;
+
+    private EventsList()
+    {
+        Events = new Dictionary<int, Event>();
+    }
+
+    public static EventsList GetInstance()
+    {
+        lock (mutex)
+        {
+            if (instance == null)
+            {
+                instance = new EventsList();
+            }
+            return instance;
+        }
+    }
+
+    public void AddEvent(Event _event)
+    {
+        Events.Add(_event.ID, _event);
+    }
+}
